19640a199adc37595c4bdd4cbd7a2ee2afd6f58d
Ashley Liu <112028848+ashlxyliu@users.noreply.github.com>
2023-09-03 22:25:06 -0400

    Add files via upload

---
+++ katya.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request,send_file
+
+app = Flask()
+
+@app.route('/my-first-api', methods = ['GET'])
+def hello():
+
+    name = request.args.get('name')
+
+    if name is None:
+        text = 'Hello!'
+
+    else:
+        text = 'Hello ' + name + '!'
+
+    return text
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
